5bec207f19b273fdba0bb12e12a509f617be4505
dsilvau <125933418+dsilvau@users.noreply.github.com>
2023-03-18 18:46:31 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -14,6 +14,7 @@ tbl1 = pd.read_csv("tbl1.tsv", sep="\t")
 tbl2 = pd.read_csv("tbl2.tsv", sep="\t")
 tbl0
 def pregunta_01():
+    tbl0 = pd.read_csv("tbl0.tsv", sep="\t")
     Filas = tbl0.shape[0]
     """
     ¿Cuál es la cantidad de filas en la tabla `tbl0.tsv`?
